22e3313062f30ba2b919f0621737ff7f0efb4d37
lhn <luohaonan@haique-tech.com>
2023-03-17 15:59:59 +0800

    add:增加jni模块

--- app/src/main/java/com/android/hnbase/di/ObjectDirector.kt
+++ app/src/main/java/com/android/hnbase/di/ObjectDirector.kt
@@ -4,6 +4,10 @@ import android.util.Log
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Modifier
 
+/**
+ * 这个类提供的主要功能是通过类名来获取类对象实例。
+ * 核心逻辑是保存一个注册工厂的list，当要获取一个类的对象时，遍历所有的工厂以及其方法，得到返回类型与所需对象所属类相同的方法（public）并执行来获取指定的类对象。
+ */
 class ObjectDirector(val appComponents: AppComponents):Director {
 //    private val providers: List<ObjectProvider> = ArrayList<ObjectProvider>()
 
@@ -12,13 +16,16 @@ class ObjectDirector(val appComponents: AppComponents):Director {
 
     private val TAG = "Director"
 
+    /**
+     * 需要注意的是所有注册的类中不能有多个public方法返回相同的类型
+     */
     override fun registerProvider(provider: ObjectProvider?) {
         provider?.director = this
         providers.add(provider!!)
     }
 
     /**
-     * 获取类对象
+     * 获取类对象，需要注意的是所有注册的类中不能有多个public方法返回相同的类型
      */
     override fun <T> getInstance(clazz: Class<T>?): T {
         if (providers.isEmpty()) {
