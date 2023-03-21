c1ba849380cd09093905e819efd17e7bdc5502b9
lhn <luohaonan@haique-tech.com>
2023-03-21 17:39:29 +0800

    chore:倒计时工具增加注释

--- common/src/main/java/com/android/common/utils/TimeUtils.kt
+++ common/src/main/java/com/android/common/utils/TimeUtils.kt
@@ -5,9 +5,24 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
 import java.util.concurrent.TimeUnit
 
+/**
+ * 时间相关工具类
+ * @property TAG String?
+ * @property disposable Disposable?
+ */
 class TimeUtils {
+
     val TAG = TimeUtils::class.simpleName
+
     var disposable:Disposable? = null
+
+    /**
+     * 倒计时
+     * @param timeLen Int 倒计时总时长
+     * @param listener CountDownListener 倒计时监听器
+     * @param isEvery Boolean 是否每数一次都执行回调
+     * @param isCountBack Boolean 是否倒数
+     */
     fun countDownSeconds(timeLen:Int, listener: CountDownListener, isEvery:Boolean, isCountBack:Boolean){
         var count = timeLen
         disposable = Observable.interval(1,TimeUnit.SECONDS)
@@ -36,11 +51,27 @@ class TimeUtils {
             }
     }
 
+    /**
+     * 重载，默认为不每次执行+倒数
+     * @param timeLen Int
+     * @param listener CountDownListener
+     */
     fun countDownSeconds(timeLen:Int,listener: CountDownListener){
         countDownSeconds(timeLen,listener,false,true)
     }
 
+    /**
+     * 倒计时监听器
+     */
     interface CountDownListener{
+
+        /**
+         * 当倒计时触发式时调用
+         * @param timeLen Int 时间值
+         * @param thing Any?  附加对象(可选)
+         * @param isOver Boolean 是否结束
+         */
         fun onCountDown(timeLen:Int,thing:Any?,isOver:Boolean)
+
     }
 }
